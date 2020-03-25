a2f703c8484b1c69b88834914cfb51b4b6e1b426
Grebenuk Denis <32875166+yCatDev@users.noreply.github.com>
2020-03-25 14:36:42 +0300

    Set-up

---
+++ Minima/Minima.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Minima
+{
+    public class Minima
+    {
+        
+    }
+}
---
+++ YetAnotherSnake/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Minima;
+
+namespace YetAnotherSnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
